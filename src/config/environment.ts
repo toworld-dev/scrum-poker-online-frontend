4487eda73bcddeeff25fd49a543db1a229d179fd
Henrique Weiand <henriqueweiand@gmail.com>
2020-05-21 21:29:48 -0300

    fix url prod

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,9 +2,9 @@ export default {
   httpURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
-      : `https://toworld.dev/`,
+      : `https://toworld-scrum-poker-online.herokuapp.com/`,
   socketURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
-      : `https://toworld.dev/`,
+      : `https://toworld-scrum-poker-online.herokuapp.com/`,
 };
