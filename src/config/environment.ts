21d7a9b617dbcce46d7a75924753ab503d2e1f65
Henrique Weiand <henriqueweiand@gmail.com>
2020-05-26 14:26:37 -0300

    test deploy

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,10 +2,10 @@ export default {
   httpURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
-      : `https://toworld-scrum-poker-online.herokuapp.com/`,
+      : process.env.PROD_URL,
   socketURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
-      : `https://toworld-scrum-poker-online.herokuapp.com/`,
-  analyticsCode: 'UA-146436412-2',
+      : process.env.PROD_URL,
+  analyticsCode: process.env.ANALYTICS,
 };
