fda6961d943864efd98685118f00a94ce18e4f7f
Henrique Weiand <henriqueweiand@gmail.com>
2020-05-26 16:43:13 -0300

    fix

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,10 +2,10 @@ export default {
   httpURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
-      : `https://api.toworld.dev/`,
+      : `https://scrumpoker-api.toworld.dev/`,
   socketURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
-      : `https://api.toworld.dev/`,
+      : `https://scrumpoker-api.toworld.dev/`,
   analyticsCode: 'UA-146436412-2',
 };
